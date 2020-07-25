69f6750adb9a1b4d03694be935f7bb2ba1f7df20
Zsolt Hajdu <z_hajdu@yahoo.com>
2020-07-25 12:26:02 -0700

    Final updates

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ plot1 <- function() {
   names(twodays) <- header
   
   hist( twodays$Global_active_power, col="red",main="Global Active Power",
-        xlab="Global Active Power (kilowatts)", ylab="kilowatts")
+        xlab="Global Active Power (kilowatts)")
   
   dev.copy( png, file="plot1.png",width = 480, height = 480, units = "px")
   dev.off()
